1fa3f0b976f117dc64c3216472ca41c821437af4
Prasann Viswanathan <62383938+iamprasann@users.noreply.github.com>
2020-12-05 22:21:57 +0530

    Update MUX_4to1.vhd

--- MUX_4to1.vhd
+++ MUX_4to1.vhd
@@ -1,13 +1,16 @@
 library IEEE;
 use ieee.std_logic_1164.all;
 
+--defining the MUX option choosing entity
 entity MUX_4to1 is
 	port(sel1,sel0,i0,i1,i2,i3:in bit;O4:out bit);
 end entity MUX_4to1;
 
+--stating the architecture of our MUX operation chooser
 architecture MUX_behaviour of MUX_4to1 is
 	signal o0,o1,o2,o3,n1,n0:bit;
 	
+	--adding required components to MUX
 	component AND_gate_3to1 is
 	port(I1,I2,I3:in bit;O1:out bit);
 	end component;
@@ -20,6 +23,7 @@ architecture MUX_behaviour of MUX_4to1 is
 	port(I5,I6,I7,I8:in bit;O3:out bit);
 	end component;
 	
+	--finally creating the MUX behaviour using the components
 	begin
 	
 	Chip_NOT1: my_not
@@ -44,4 +48,3 @@ architecture MUX_behaviour of MUX_4to1 is
 	port map(o0,o1,o2,o3,O4);
 	
 	end MUX_behaviour;
-	
