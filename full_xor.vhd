e180dac5d62b1c8686bdae5623cdba06e7b31c04
Prasann Viswanathan <62383938+iamprasann@users.noreply.github.com>
2020-12-05 22:26:04 +0530

    Update full_xor.vhd

--- full_xor.vhd
+++ full_xor.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use ieee.std_logic_1164.all;
 
+--defining the full XOR operation entity
 entity full_xor is
 	port( 
 	xa0, xa1, xa2, xa3, xa4, xa5, xa6, xa7, xa8, xa9, xa10, xa11, xa12, xa13, xa14, xa15,
@@ -8,12 +9,15 @@ entity full_xor is
 	xr0, xr1, xr2, xr3, xr4, xr5, xr6, xr7, xr8, xr9, xr10, xr11, xr12, xr13, xr14, xr15 : out bit);
 end entity;
 
+--defining architecture of full XOR
 Architecture Struct of full_xor is
 
-component my_xor --declaring my_xor component to use for XOR operations
+--adding the sole required component
+component my_xor 
 port(S,T : in bit; U: out bit);
 end component;
 
+--behaviour involves individually XOR-ing a_i with b_i
 begin
 
 	xor0 : my_xor
