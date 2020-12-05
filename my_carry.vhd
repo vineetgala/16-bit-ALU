96ef8676119138077a9c451d39c76367aa518675
Prasann Viswanathan <62383938+iamprasann@users.noreply.github.com>
2020-12-05 22:27:43 +0530

    Update my_carry.vhd

--- my_carry.vhd
+++ my_carry.vhd
@@ -1,24 +1,26 @@
---G,P
-
 library IEEE;
 use ieee.std_logic_1164.all;
 
+--defining carry computation entity based on generate and propogate signals
 entity my_carry is
 	port (P_prev, P_curr, G_prev, G_curr : in bit;P_out, G_out : out bit);
 end entity;
 
+--defining carry architecture
 architecture behaviour of my_carry is
 
 signal temp:bit;
 
-component my_or --declaring my_or component to use for OR operations
+--adding AND and OR gates as components
+component my_or 
 port(i_or1,i_or2 : in bit; o_myor: out bit);
 end component;
 
-component my_and --declaring my_and component to use for AND operations
+component my_and 
 port(i_and1,i_and2: in bit; o_myand: out bit);
 end component;
 
+--computing Carry using generate and propogate with AND OR gates
 begin
 	
 	temp_gen : my_and
